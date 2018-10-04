bde07468250ee457fdb9362d0b8b2cfa02bbd489
杨立景 <yanglijing@mafengwo.com>
2018-10-04 22:14:41 +0800

    first commit

---
+++ config/base.php
@@ -0,0 +1,103 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'ymzsyVMMGQS42[@$ylC305(',
+  'client_id' => '2972b5d1-6932-460b-88e1-faeb6d9f6c7d',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAADXzZ3ifr-GRbDT45zNSEFEiyNvheTkuJD6FzZiY34ahU31WwNGGDYQx9DGlWLnaayrwUU3ryr9Df5CiOIp3hqViLweQ3bu_BeDmtA58_Dfn6TEH4SnlNdJaQMy_mW0FF6kY4v6Tzwaj_9xsgxX1Pk3VgIhppSYOYHLgqBuFTKu-MDw2P_QNiwSixDVGj8NqJWQa_J-dE85F4xN7KhSpBELBdsXzfm_SVORkbzAkLDGck3GsCYfjzEg6vYVc11LXwvSESKUVYNuhyCR7pHxMcSurkQGpWaOaEvKGv3QXRz0r5FoP9eYVQsrNYWX7R0eWnWdU_MfK2m9FaJ3PCCnJ0nLBs8FTWTY5QD4BOcUpX5bTXSiV6LNYJWnAlsFRP7_GERp_DAiYRm0csw2DX19A9sndzbrraezenl00xI5PugSFObCoWkEAbIR2NRdnOS4a5Zf_jGnVYvmopbBllwTGkFaqWfKmcDmTipE1TXA9n_NQi-Tvia9iWvzGa-2rFn9-fOcu3RuvwBXFrEQG66VPNJF6u_sAz1KfqenzD4KOojNFo_UvhieCW3WsqllgQM46D7d90Hns02zO5VBiNYHGVhZWieCH96NIEFgh2c3aHm26602Fgmv7uT8kDpoPYBuyrZitTcQOC9skBcrMAF6N4D9DGSxnYPzN8xpcHN4HIt4LxnE3l8ac4WXAWXasKn28NFNzjJoaaf_Th0vy5O1m1QGBqnQVToXjQUz8iGIJ8S8RLtkcmX2ulx98gw0qxFSkuH97Yu11y1qjmR5f9kOWgVSECgX2xo0bUwmu2-t3BfpaiAA',
+);
